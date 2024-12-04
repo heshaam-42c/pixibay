7023c4814a90c25b34d12db6cf638617e2f1e531
heshaam-42c <heshaam.attar@42crunch.com>
2024-12-04 13:44:09 -0800

    update script

--- pixiReset.sh
+++ pixiReset.sh
@@ -14,9 +14,9 @@ if [ "$1" == "build" ]; then
     docker-compose -f $docker_yaml up -d --build
 fi
 
-sleep 5
-
 echo "Creating Users..."
+
+sleep 5
 
 # JSON data for the API request 
 json_data_user_inbound='{"user": "scanuser@test.com","pass": "hellopixi","name": "Scan Test User","is_admin": false,"account_balance": 1000}'
@@ -24,5 +24,19 @@ json_data_user_common='{"user": "attacks-demo@acme.com","pass": "hellopixi","nam
 
 # Invoke the API using curl with POST method and passing the JSON data
 api_url="http://localhost:8090/api/user/register"
-curl_response_inbound=$(curl -s -X POST -H "Content-Type: application/json" -d "$json_data_user_inbound" "$api_url")
-curl_response_common=$(curl -s -X POST -H "Content-Type: application/json" -d "$json_data_user_common" "$api_url")
+curl_response_inbound=$(curl -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "$json_data_user_inbound" "$api_url")
+curl_response_common=$(curl -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "$json_data_user_common" "$api_url")
+
+if [ "$curl_response_inbound" == "200" ]; then
+    echo "Inbound User Created"
+else
+    echo "Inbound User Creation Failed, got $curl_response_inbound"
+fi
+
+if [ "$curl_response_common" == "200" ]; then
+    echo "Common User Created"
+else
+    echo "Common User Creation Failed, got $curl_response_common"
+fi
+
+echo "Pixi is up and running"
