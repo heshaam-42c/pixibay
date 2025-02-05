d52c9d72b1f293f66d2f75ed3f77937732b05c12
heshaam-42c <heshaam.attar@42crunch.com>
2025-02-05 12:58:12 -0800

    update script

--- pixiReset.sh
+++ pixiReset.sh
@@ -24,19 +24,28 @@ json_data_user_common='{"user": "attacks-demo@acme.com","pass": "hellopixi","nam
 
 # Invoke the API using curl with POST method and passing the JSON data
 api_url="http://localhost:8090/api/user/register"
-curl_response_inbound=$(curl -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "$json_data_user_inbound" "$api_url")
-curl_response_common=$(curl -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "$json_data_user_common" "$api_url")
 
-if [ "$curl_response_inbound" == "200" ]; then
-    echo "Inbound User Created"
-else
-    echo "Inbound User Creation Failed, got $curl_response_inbound"
-fi
+# Save only the response code
+# curl_response_inbound=$(curl -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "$json_data_user_inbound" "$api_url")
+# curl_response_common=$(curl -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "$json_data_user_common" "$api_url")
 
-if [ "$curl_response_common" == "200" ]; then
-    echo "Common User Created"
-else
-    echo "Common User Creation Failed, got $curl_response_common"
-fi
+# if [ "$curl_response_inbound" == "200" ]; then
+#     echo "Inbound User Created"
+# else
+#     echo "Inbound User Creation Failed, got $curl_response_inbound"
+# fi
+
+# if [ "$curl_response_common" == "200" ]; then
+#     echo "Common User Created"
+# else
+#     echo "Common User Creation Failed, got $curl_response_common"
+# fi
+
+# DEBUG: Save entire response
+curl_response_inbound=$(curl -s -o /dev/null -D - -X POST -H "Content-Type: application/json" -d "$json_data_user_inbound" "$api_url")
+curl_response_common=$(curl -s -o /dev/null -D - -X POST -H "Content-Type: application/json" -d "$json_data_user_common" "$api_url")
+
+echo "Inbound User \n$curl_response_inbound"
+echo "Common User \n$curl_response_common"
 
 echo "Pixi is up and running"
