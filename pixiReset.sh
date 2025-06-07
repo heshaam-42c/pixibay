817054482333e1aa23d3962f9e34b0b91446475a
heshaam-42c <heshaam.attar@42crunch.com>
2025-06-06 22:02:39 -0700

    update pixi script

--- pixiReset.sh
+++ pixiReset.sh
@@ -8,6 +8,12 @@ if [ $# -lt 1 ]; then
     docker-compose -f $docker_yaml up -d
 fi
 
+if [ "$1" == "down" ]; then
+    echo "Shutting down Pixi"
+    docker-compose -f $docker_yaml down
+    exit 0
+fi
+
 if [ "$1" == "build" ]; then
     echo "Building Pixi"
     docker-compose -f $docker_yaml down
@@ -23,29 +29,29 @@ json_data_user_inbound='{"user": "scanuser@test.com","pass": "hellopixi","name":
 json_data_user_common='{"user": "attacks-demo@acme.com","pass": "hellopixi","name": "Attack User","is_admin": false,"account_balance": 1000}'
 
 # Invoke the API using curl with POST method and passing the JSON data
-api_url="http://localhost:8090/api/user/register"
+api_register_url="http://localhost:8090/api/user/register"
 
 # Save only the response code
-# curl_response_inbound=$(curl -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "$json_data_user_inbound" "$api_url")
-# curl_response_common=$(curl -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "$json_data_user_common" "$api_url")
+curl_response_inbound=$(curl -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "$json_data_user_inbound" "$api_register_url")
+curl_response_common=$(curl -s -o /dev/null -w "%{http_code}" -X POST -H "Content-Type: application/json" -d "$json_data_user_common" "$api_register_url")
 
-# if [ "$curl_response_inbound" == "200" ]; then
-#     echo "Inbound User Created"
-# else
-#     echo "Inbound User Creation Failed, got $curl_response_inbound"
-# fi
+if [ "$curl_response_inbound" == "200" ]; then
+    echo "Inbound User Created"
+else
+    echo "Inbound User Creation Failed, got $curl_response_inbound"
+fi
 
-# if [ "$curl_response_common" == "200" ]; then
-#     echo "Common User Created"
-# else
-#     echo "Common User Creation Failed, got $curl_response_common"
-# fi
+if [ "$curl_response_common" == "200" ]; then
+    echo "Common User Created"
+else
+    echo "Common User Creation Failed, got $curl_response_common"
+fi
 
 # DEBUG: Save entire response
-curl_response_inbound=$(curl -s -o /dev/null -D - POST -H "Content-Type: application/json" -d "$json_data_user_inbound" "$api_url")
-curl_response_common=$(curl -s -o /dev/null -D - POST -H "Content-Type: application/json" -d "$json_data_user_common" "$api_url")
+# curl_response_inbound=$(curl -s -o /dev/null -D - POST -H "Content-Type: application/json" -d "$json_data_user_inbound" "$api_register_url")
+# curl_response_common=$(curl -s -o /dev/null -D - POST -H "Content-Type: application/json" -d "$json_data_user_common" "$api_register_url")
 
-echo "Inbound User \n$curl_response_inbound"
-echo "Common User \n$curl_response_common"
+# echo "Inbound User \n$curl_response_inbound"
+# echo "Common User \n$curl_response_common"
 
 echo "Pixi is up and running"
